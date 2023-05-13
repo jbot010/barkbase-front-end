a8e6c58c4050eb335d7a7076c508d7ac4f07b6b1
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-13 10:22:23 -0500

    Access more dog attributes

--- src/components/DogCard/DogCard.jsx
+++ src/components/DogCard/DogCard.jsx
@@ -7,7 +7,7 @@ import styles from './DogCard.module.css'
 const DogCard = ({ dog }) => {
 
   return (
-    <li>{dog.name}</li>
+    <li>{dog.name} : {dog.breed} : Age {dog.age} : Size {dog.size} : {dog.color}</li>
   )
 }
 
