c206e8c99e572f07563c5f4b6d689a15658dbffd
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-13 10:03:06 -0500

    basic front and back end integration

--- src/pages/DogList/DogList.jsx
+++ src/pages/DogList/DogList.jsx
@@ -1,13 +1,15 @@
 // npm modules
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
 //import DogCard
+import DogCard from '../../components/DogCard/DogCard'
 
 const DogList = ( props ) => {
   return (
     <main className='dog-list'>
       <h1>Dog List</h1>
-      {!props.dogs.length && <h2>Dog List Empty</h2>}
+      {console.log(props.dogs)}
+      {/* {!props.dogs.length && <h2>Dog List Empty</h2>} */}
       <ul>
         {props.dogs.map((dog) => (
           <DogCard dog={dog} key={dog._id} />
