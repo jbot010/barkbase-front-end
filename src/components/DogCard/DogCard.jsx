1bafb4559060d25e3583108298f3c7370f21ffbe
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-15 12:22:02 -0500

    trash

--- src/components/DogCard/DogCard.jsx
+++ src/components/DogCard/DogCard.jsx
@@ -7,9 +7,30 @@ import styles from './DogCard.module.css'
 const DogCard = ({ dog }) => {
   return (
     <Link to={`/dogs/${dog._id}`}>
-      <li>{dog.name} : {dog.breed} : Age {dog.age} : Size {dog.size} : {dog.color}</li>
+      <article>
+        <div id="dogcard-img"></div>
+        <div id="dogcard-content">
+          
+        </div>
+
+      </article>
     </Link>
   )
 }
 
 export default DogCard
+
+<li>{dog.name} : {dog.breed} : Age {dog.age} : Size {dog.size} : {dog.color}</li>
+
+// <Link to={`/blogs/${blog._id}`} state={blog}>
+// <article className={styles.container}>
+//   <header>
+//     <span>
+//       <h1>{ blog.title }</h1>
+//       <Icon category={blog.category} />
+//     </span>
+//     <AuthorInfo content={blog} />
+//   </header>
+//   <p>{ blog.text }</p>
+// </article>
+// </Link>
