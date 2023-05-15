53e4c18021755dcd59219bc1befffa48c6ce814b
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-15 11:38:18 -0500

    Begin Styling

--- src/pages/DogList/DogList.jsx
+++ src/pages/DogList/DogList.jsx
@@ -4,17 +4,21 @@
 //import DogCard
 import DogCard from '../../components/DogCard/DogCard'
 
+import styles from './DogList.module.css'
+
 const DogList = ( props ) => {
   return (
     <main className='dog-list'>
       <h1>Dog List</h1>
       {console.log(props.dogs)}
       {/* {!props.dogs.length && <h2>Dog List Empty</h2>} */}
-      <ul>
-        {props.dogs.map((dog) => (
-          <DogCard dog={dog} key={dog._id} />
-        ))}
-      </ul>
+      <section>
+        <ul className='dog-list-container'>
+          {props.dogs.map((dog) => (
+            <DogCard dog={dog} key={dog._id} />
+          ))}
+        </ul>
+      </section>
     </main>
   )
 }
