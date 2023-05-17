d7768dcf1d0cb8c8f04d10f852ec31658f4feae5
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-17 14:17:13 -0500

    LandingTemp

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,10 +5,7 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>sup, {user ? user.name : 'dawg'}</h1>
-      <img src="https://textfiles.com/underconstruction/ParisCafe9362pikaconstruct.gif" alt="" />
-      <img src="https://textfiles.com/underconstruction/CoCollegeParkDorm4438underconstruction.gif" alt="" />
-      <img src="https://textfiles.com/underconstruction/HeHeartlandValley7159construction.gif" alt="" />
-      <img src="https://textfiles.com/underconstruction/CapitolHillSenate7861construct.gif" alt="" />
+      <h2>~~~ under construction ~~~~</h2>
     </main>
   )
 }
