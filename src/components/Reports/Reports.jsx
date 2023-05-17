47830abc341c308482e012ee784adcbe4417c2b7
Joe Caruso <carusoj7@gmail.com>
2023-05-17 14:32:13 -0500

    map through reports

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -2,15 +2,18 @@ import ReportCard from "../ReportCard/ReportCard"
 import styles from './Reports.module.css'
 
 const Reports = ( props ) => {
+  if(!props.reports) return <h4>No Reports</h4>
   return ( 
     <div className={styles.reportList}>
+      {props.reports.map((report) => (
+        <ReportCard
+          key={report._id}
+          report={report}
+          user={props.user}
+          dogId={props.dogId}
+        />
+      ))}
       <button>NEW REPORT (temp)</button>
-      <ReportCard />
-      <ReportCard />
-      <ReportCard />
-      {/* {props.reports.map((report) => (
-        <ReportCard key={report._id} />
-      ))} */}
     </div>
   )
 }
