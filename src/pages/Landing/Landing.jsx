250e30f221728e6b257a2380dffa8ea8aaddb5cd
Andrew Rentschler <98727929+AndrewRentschler@users.noreply.github.com>
2023-05-17 14:16:12 -0500

    QuikFix

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,10 +5,10 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>sup, {user ? user.name : 'dawg'}</h1>
-      <img src="http://textfiles.com/underconstruction/ParisCafe9362pikaconstruct.gif" alt="" />
-      <img src="http://textfiles.com/underconstruction/CoCollegeParkDorm4438underconstruction.gif" alt="" />
-      <img src="http://textfiles.com/underconstruction/HeHeartlandValley7159construction.gif" alt="" />
-      <img src="http://textfiles.com/underconstruction/CapitolHillSenate7861construct.gif" alt="" />
+      <img src="https://textfiles.com/underconstruction/ParisCafe9362pikaconstruct.gif" alt="" />
+      <img src="https://textfiles.com/underconstruction/CoCollegeParkDorm4438underconstruction.gif" alt="" />
+      <img src="https://textfiles.com/underconstruction/HeHeartlandValley7159construction.gif" alt="" />
+      <img src="https://textfiles.com/underconstruction/CapitolHillSenate7861construct.gif" alt="" />
     </main>
   )
 }
