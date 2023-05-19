0de1f75db83e86400943fe1545d2dc97b5d020d8
ttiurt <truittzach33@gmail.com>
2023-05-18 22:31:12 -0400

    another prof card update

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -12,25 +12,24 @@ import styles from './ProfileCard.module.css'
 const ProfileCard = ({ profile }) => {
   console.log(profile);
   return (
-      <Box 
-        className={styles.profileCard}
-        backgroundColor="white"
-        display="flex" 
-        justifyContent="spread-evenly" 
-        alignItems="center"
-        flexDirection="row"
-        borderRadius="12px"
-        minWidth="70%"
-        padding="6px"
-        marginTop="6px"
-        // sx={{ width: "50%" }}
-        >
-          <Box ><img src={profile.photo ? profile.photo : "/BWdog_icon.png"} alt="" className={styles.profileCardImg}/></Box>
-          <Box className={styles.profileCardContent} flexGrow="3">{profile.name}</Box>
-        <Button variant="contained" size="small" href={`/profiles/${profile._id}`}>DETAILS</Button>
-        <button><DeleteForeverIcon /></button>
-
-      </Box>
+    <Box 
+      className={styles.profileCard}
+      backgroundColor="white"
+      display="flex" 
+      justifyContent="spread-evenly" 
+      alignItems="center"
+      flexDirection="row"
+      borderRadius="12px"
+      minWidth="70%"
+      padding="6px"
+      marginTop="6px"
+      // sx={{ width: "50%" }}
+    >
+      <Box ><img src={profile.photo ? profile.photo : "/BWdog_icon.png"} alt="" className={styles.profileCardImg}/></Box>
+      <Box className={styles.profileCardContent} flexGrow="3">{profile.name}</Box>
+      <Button variant="contained" size="small" href={`/profiles/${profile._id}`}>DETAILS</Button>
+      <button className={styles.profDltBtn}><DeleteForeverIcon /></button>
+    </Box>
   )
 }
 export default ProfileCard
